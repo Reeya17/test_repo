884e40b20780170f078e6f10320a4b9ab2d4b3ed
Riya <Riya>
2025-07-21 15:10:25 +0200

    added gitignore

--- main.sh
+++ main.sh
@@ -1,18 +1,18 @@
-#set -a
-#source .env
-#set +a
-az group create --name rg-dev-vm2 --location westeurope
+set -a
+source .env
+set +a
+az group create --name rg-dev-vm3 --location westeurope
 az vm create \
-        --resource-group rg-dev-vm2 \
-        --name vm-dev-webserver2 \
+        --resource-group rg-dev-vm3 \
+        --name vm-dev-webserver3 \
         --image Ubuntu2404 \
         --size Standard_B1s \
-        --vnet-name vnet-dev-vm2 \
-        --subnet subnet-dev-vm2 \
-        --public-ip-address vm-dev-webserver2-ip \
-        --nsg nsg-dev-vm2 \
+        --vnet-name vnet-dev-vm3 \
+        --subnet subnet-dev-vm3 \
+        --public-ip-address vm-dev-webserver3-ip \
+        --nsg nsg-dev-vm3 \
         --nsg-rule SSH \
         --location westeurope \
         --admin-username azureuser \
-        --admin-password "$ADMINPASS"
-az vm open-port --port 80 --resource-group rg-dev-vm2 --name vm-dev-webserver2
+        --admin-password "$ADMINPASS3"
+az vm open-port --port 80 --resource-group rg-dev-vm3 --name vm-dev-webserver3
